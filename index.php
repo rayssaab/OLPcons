e7497759fff210e3b963df856d39e97d2887e476
Danieli898 <danibk898@gmail.com>
2022-05-11 13:58:38 -0300

    etilização

--- index.php
+++ index.php
@@ -1,25 +1,27 @@
-<!DOCTYPE html>
 <html>
-	<head>
-    <meta charset="UTF-8">
-    <title>Página de Login</title>
-    <link rel="stylesheet" href="estilo/style.css">
-</head>
-<body>    <!-- cadastro de usuário-->
-  <div class="form-group">
-        <h3 class="text-center"> Cadastro de usuário</h3><br>
-          <form name="formLogin" method="POST" action="arquivos/insert.php">
-            <label for="usuario">Usuário</label>
-            <input class="form-control" type="text" name="usuario" placeholder="Insira seu nome">
-            <label for="senha">Senha</label>
-            <input class="form-control" type="password" name="senha" placeholder="Insira sua senha">
-            <input class="btn" type="submit" name="Entrar">
-      </div>
-      <?php 
-        if(isset($_GET['erro'])) {
-          echo $_GET['erro'];
-        }
-      ?>
-    </form>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <title>PAGINA INICIAL</title>
+  </head>
+  <body>
+    <header>
+      <h1>Home</h1>
+    </header>
+    <nav class="container">
+      <ul class="menu">
+        <li class="item-menu">Registro de Marca</li>
+        <li class="item-menu">Registro de Produto</li>
+        <li class="item-menu">Registro de Cliente</li>
+        <li class="item-menu">Registro de Pedido</li>
+        <li class="item-menu">Registro de vendas</li>
+        <li class="item-menu">Relatório de Vendas</li>
+        <li class="item-menu">Controle de Estoque</li>
+      </ul>
+    </nav>
   </body>
 </html>
+
---
+++ pages/login.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+     <link rel="stylesheet" href="css/estilo.css" type="text/css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<title>Página de Login</title>
+  </head>
+
+  <body>
+
+ <h3 class="login"> <label for="login">login</label></h3>
+ <form name="formLogin" method="grt" action="">
+
+<h3 class="usuario"> <label for="usuario">Usuario</label></h3>
+<input class="form-control" type="text" name="usuario" placeholder="insira seu nome">
+      <br></br>
+<h3 class="senha"> <label for="senha">Senha</label></h3>
+<input class="form-control" type="password" name="senha" placeholder="insira sua senha">
+  
+   <input class="btn btn-success" type="submit" value="Entrar">
+
+</body>
+</html>
