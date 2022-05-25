1a3bdeeb876cdaf88ad9640a96a8dab0cc0b2899
Danieli898 <danibk898@gmail.com>
2022-05-25 13:42:09 -0300

    estilo_home

--- pages/registro_produtos.php
+++ pages/registro_produtos.php
@@ -7,21 +7,21 @@
 <body>
 <div class="corpo">
     <div class="cabecalho">
-        <h1 class="titulo"><br>Registro de Produtos</h1>
+        <h1 class="titulo"><br>Registro de Produtos</h1><br>
             <div class="form-produtos"><!-- form registro de produtos-->
                 <form method="POST" action="../arquivos/produto/funcao_registro_produto.php">
                 <div class="nome-produto">
                     <label for="produto">Nome do produto a ser cadastrado:</label>
-                    <input type="text" name="produto">
+                    <input type="text" name="produto"><br>
                 </div>
                 <div class="lucratividade">
                     <label for="preco_produto">Valor do produto:</label>
                     <input type="text" name="preco_produto"><br>
                     <label for="preco_venda">Valor de revenda</label>
-                    <input type="text" name="revenda">
+                    <input type="text" name="revenda"><br>
                 </div>
                 <div class="descricao-produto">
-                    <label for="descricao-produto">Descreva o produto que será cadastrado</label>
+                    <label for="descricao-produto">Descreva o produto que será cadastrado</label><br>
                     <input type="text" name="descricao_produto"><br>
                     <input type="submit" name="Entrar">
                 </div>
