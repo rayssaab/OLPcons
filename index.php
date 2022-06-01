641208da1ebb49393baad375a8c092e65a817f62
danibk898@gmail.com <danibk898@gmail.com>
2022-06-01 11:03:52 -0300

    01/06

--- index.php
+++ index.php
@@ -1,27 +1,25 @@
 <html>
+<!DOCTYPE html>
+<html lang="pt-br">
+
   <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="css/estilo.css" type="text/css">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <title>PAGINA INICIAL</title>
-  </head>
+
+</head>
   <body>
-    <header>
-      <h1>Home</h1>
+  <header class="header">
+      <h1>Seja bem-vindo!</h1>
     </header>
-    <nav class="container">
-      <ul class="menu">
-        <li class="item-menu">Registro de Marca</li>
-        <li class="item-menu" href="../registro_produtos.php">Registro de Produto</li>
-        <li class="item-menu">Registro de Cliente</li>
-        <li class="item-menu">Registro de Pedido</li>
-        <li class="item-menu">Registro de vendas</li>
-        <li class="item-menu">Relatório de Vendas</li>
-        <li class="item-menu">Controle de Estoque</li>
-      </ul>
-    </nav>
+    <br>
+    <div class="alinhar">
+<form>
+<a class="a" href="pages/login.php">Login</a>
+    
+</form>
+</div>
   </body>
 </html>
-
--- pages/login.php
+++ pages/login.php
@@ -1,17 +1,23 @@
-<!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
+  <head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/login.css" type="text/css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <html>
 </head>
-<body>
-<form class="formLogin" method="POST" action="../arquivos/usuario/validacao.php">
-<h3 class="usuario"> <label for="usuario">Usuário:</label></h3>
-<input class="form-control" type="text" name="usuario" placeholder="insira seu nome">
-<h3 class="senha"> <label for="senha">Senha:</label></h3>
-<input class="form-control" type="password" name="senha" placeholder="insira sua senha">
-<input class="btn btn-success" type="submit" value="Entrar">
-</form>
-
-</body>
+  <body>
+  <header class="header">
+      <h1 class="text-center">Login</h1>
+    </header><br>
+    <form class="login" name="formLogin" method="post" action="">
+    <label for="usuário">Usuário</label>
+      <input class="usuario" type="text" name="usuario" placeholder="Insira seu nome"><br>
+    
+      <label for="senha">Senha</label>
+      <input class="usuario" type="password" name="senha" placeholder="Insira sua senha"><br>
+      <input class= "usuario" type= "submit" value="Entrar">
+    </form>
+  </body>
 </html>
