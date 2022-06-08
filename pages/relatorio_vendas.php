875a53b8bc5f709cde172ed5b6c8e14a2278b43b
rayssaab <rayssaaraujo422@gmail.com>
2022-06-08 14:04:11 -0300

    favicon

--- pages/relatorio_vendas.php
+++ pages/relatorio_vendas.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon" />
+<link rel="shortcut icon" href="../img/favicon.png" type="image/x-icon"/>
     <meta charset="UTF-8">
     <title></title>
 </head>
