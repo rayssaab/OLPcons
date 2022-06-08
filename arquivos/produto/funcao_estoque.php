c0ce09c2ec68793f53050fa89495721b1ae888c0
laysla <goncalveslaysla1599@gmail.com>
2022-06-08 14:10:56 -0300

    Editar produtos

--- arquivos/produto/funcao_estoque.php
+++ arquivos/produto/funcao_estoque.php
@@ -10,38 +10,32 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     include_once ("../conexao.php");
 
     $nome_produto = $_POST['produto'];
-    $valor_pago = (float)$_POST['valor_pago'];
+
+        /*Checando se o produto foi informado*/
+        if($nome_produto == ""){
+            header("Location: ../../pages/estoque.php?erro=O nome do produto é inválido");
+        }
+        
+    $valor_pago = $_POST['valor_pago'];
     $valor_revenda = $_POST['valor_revenda'];
 
-    /*print_r("Preço do produto: R$".$preco_produto."<br>");
-    print_r("Preço da revenda: R$".$revenda."<br>");*/
-
-    /*print_r($lucro."<br>");*/
-    
-    /*Checando se o produto foi informado*/
-    if($nome_produto == ""){
-    	header("Location: ../../pages/registro_produtos.php?erro=O produto é inválido");
-    }
-    
     /* Checando se o valor pago e o valor de revenda foram inseridos */
 
-    
-    if(empty($valor_pago) || $valor_pago == 0){
-        // header("Location: ../../pages/estoque.php?erro=O valor do produto deve ser preenchido");
-        echo $valor_pago;
-        break;
-    }else if($valor_revenda ==""){
-        header("Location: ../../pages/registro_produtos.php?erro=O valor de revenda deve ser preenchido");
+    if($valor_pago <=0 || $valor_pago == 0){
+        header("Location: ../../pages/estoque.php?erro=O valor do produto não é valido");
+        
+    }else if($valor_revenda < 0){
+        header("Location: ../../pages/estoque.php?erro=O valor de revenda não é valido");
     }
-    
+
     /*print_r("Nome: ".$nome_produto);
     print_r("<br> Valor: ".$valor_pago);
     print_r("<br> Revenda: ".$valor_revenda);
     */
+
     $lucro=$valor_revenda-$valor_pago;
-    if($lucro < $valor_pago){
-        print_r("Seu lucro está abaixo do valor do produto
-        <a href='../../pages/registro_produtos.php'>Editar</a>");
+    if($lucro <= -1){
+        header("Location: ../../pages/estoque.php?erro=");
     }
     
     $cadastro_produto = $conn->query("INSERT INTO estoque(nome_produto, valor_pago, valor_revenda, lucro_produto) VALUE ('$nome_produto','$valor_pago','$valor_revenda', '$lucro')");
