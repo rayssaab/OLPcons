f6fbe22fef821d88139c6575cacbadc77a122f4d
laysla <goncalveslaysla1599@gmail.com>
2022-06-09 11:11:34 -0300

    Edição de produtos

--- arquivos/produto/saveEdit_produto.php
+++ arquivos/produto/saveEdit_produto.php
@@ -10,7 +10,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $lucro_produto=$valor_revenda-$valor_pago;
     
-    //print_r("Este é o ID do produto:".$id_estoque."<br>Nome do produto: ".$nome_produto."<br>Valor do produto: ".$valor_pago."<br>Valor de revenda:".$valor_revenda."<br>Lucro do produto;".$lucro_produto);
+    print_r("Este é o ID do produto:".$id_estoque."<br>Nome do produto: ".$nome_produto."<br>Valor do produto: ".$valor_pago."<br>Valor de revenda:".$valor_revenda."<br>Lucro do produto;".$lucro_produto);
 
     $sqlUpdate= "UPDATE estoque SET nome_produto='$nome_produto', valor_revenda='$valor_revenda', valor_pago='$valor_pago' lucro_produto='$lucro_produto' WHERE id_estoque='$id' ";
 
