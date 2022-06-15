1a02023d6054c4ba76dd548a96a1fb2f913eebd9
rayssaab <rayssaaraujo422@gmail.com>
2022-06-15 10:38:39 -0300

    r

--- pages/home.php
+++ pages/home.php
@@ -26,13 +26,12 @@
     </header><br>
     <nav class="container">
       <ul class="menu">
-      <button><li> <a class="item-menu" href="../pages/registro_marcas.php">Registro de Marca</a></li></button>
       <button><li> <a class="item-menu" href="../pages/estoque.php">Estoque</a></li></button>
+      <button><li> <a class="item-menu" href="../pages/registro_marcas.php">Registro de Marca</a></li></button>
       <button><li> <a class="item-menu" href="../pages/registro_produtos.php">Registro de Cliente</a></li></button>
       <button><li> <a class="item-menu" href="../pages/registro_pedidos.php">Registro de Pedido</a></li></button>
       <button><li> <a class="item-menu" href="../pages/registro_vendas.php">Registro de Vendas</a></li></button>
       <button><li> <a class="item-menu" href="../pages/relatorio_vendas.php">Relatório de Vendas</a></li></button>
-      <button><li> <a class="item-menu" href="../pages/controle_estoque.php">Controle de Estoque</a></li></button>
       <button><li> <a class="item-menu" href="../arquivos/usuario/sair.php" >Sair do sistema</a></li></button>
       </ul>
     </nav>
