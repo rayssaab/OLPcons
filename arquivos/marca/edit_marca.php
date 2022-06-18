9cb50541ca3d785acc52d42e3bc295a0f812f8ce
rayssaab <rayssaaraujo422@gmail.com>
2022-06-17 23:12:54 -0300

    r

--- arquivos/marca/edit_marca.php
+++ arquivos/marca/edit_marca.php
@@ -2,10 +2,7 @@
 include_once('../conexao.php');
 
 if(!empty($_GET['id'])){
-
     $id= $_GET['id'];
-
-    //Procurando id na tabela
     $sqlSelect = "SELECT * FROM marca WHERE id_marca=$id";
     $resulte_banco = $conn->query($sqlSelect);
 
@@ -22,7 +19,7 @@ if($resulte_banco->num_rows>0){
 <html>
 <head>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-  <link rel="stylesheet" href="../../css/r_produtos.css" type="text/css">
+  <link rel="stylesheet" href="../../css/r_marcas.css" type="text/css">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
@@ -40,6 +37,6 @@ if($resulte_banco->num_rows>0){
     </div>
         <input type="hidden" name="id_marca" value="<?php echo "$id"?>">
         <input type="submit" value="Atualizar"> 
-        <a class="a" href="../../pages/registro_marca.php">Voltar</a>
+        <a class="a" href="../../pages/registro_marcas.php">Voltar</a>
 </body>
 </html>
--- arquivos/marca/saveEdit_marca.php
+++ arquivos/marca/saveEdit_marca.php
@@ -5,7 +5,7 @@
             $nome_marca = $_POST['nome_marca'];
             $lucratividade = $_POST['lucratividade'];
             print_r("ID da marca:".$id."<br>Nome da marca: ".$nome_marca."<br>Lucratividade: ".$lucratividade);
-            $sqlUpdate = "UPDATE marca SET nome_marca='$nome_marca', lucratividade='$lucratividade' ";
+            $sqlUpdate = "UPDATE marca SET nome_marca='$nome_marca', lucratividade='$lucratividade' WHERE id_marca='$id' ";
             $result= $conn->query($sqlUpdate);
             header("Location:../../pages/registro_marcas.php");
     }
